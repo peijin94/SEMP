b52920bd867dee3f59d5c3da5b145b2091fe68b1
Peijin Zhang <peijin@ustc.edu>
2023-04-30 23:50:24 +0300

    rename package name

--- pySEMP/README.md
+++
@@ -1,7 +0,0 @@
-# Python version of SEMP
-
-TODOlist:
-
-- Data interface to load .cdf and .mat data
-- Computational part (PSO algorithm)
-- Plot the results
--- pysemp/pysemp.py
+++ pysemp/pysemp.py
@@ -2,10 +2,10 @@ import matplotlib.dates as mdates
 import numpy as np
 from scipy.optimize import fsolve 
 
-AU2km = 149597870.7;
-r_earth2km  = 6371;
-r_sun2km  = 695700;
-v_light2km_s = 3e5;
+AU2km = 149597870.7
+r_earth2km  = 6371
+r_sun2km  = 695700
+v_light2km_s = 3e5
 
 
 def findLeadingPoint(x,y,ratio_x=0.7,thresh_std=0):
